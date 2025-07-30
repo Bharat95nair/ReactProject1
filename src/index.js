9167a622255f69cb18af4d78be36dbdc4ee2dfd2
Bharat Nair <bhatunair@example.com>
2025-07-30 10:44:21 +0530

    Reverting the changes made

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext';
 import { UserProvider } from './context/UserContext';
 
@@ -15,13 +15,13 @@ import { UserProvider } from './context/UserContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));            
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <UserProvider>
           <App />
         </UserProvider>
       </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
   
 );
